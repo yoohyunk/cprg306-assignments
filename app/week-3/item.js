431f4e13746291df538657e5fa894ccf0611f75f
yoohyunk <163964321+yoohyunk@users.noreply.github.com>
2025-02-04 00:39:47 -0700

    week3 (#2)

---
+++ app/week-3/item.js
@@ -0,0 +1,10 @@
+export function Item({name, quantity, category}){
+   
+    return(
+        <div className="flex flex-col gap-4 p-4 bg-yellow-100 text-black rounded-lg">   
+            <div>Name: {name}</div>
+            <div>Quantity: {quantity}</div>
+            <div>Category: {category}</div>
+        </div>
+    );
+}
